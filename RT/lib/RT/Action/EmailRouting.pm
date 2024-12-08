5f28c30faebf6edf6c3e577b1c153712887d8449
Leonid Korokh <lkorokh@gmail.com>
2024-12-08 18:42:57 +0000

    feat: add logging to ticket routing action

--- RT/lib/RT/Action/EmailRouting.pm
+++ RT/lib/RT/Action/EmailRouting.pm
@@ -21,12 +21,17 @@ sub Commit {
                         '\@trust\.ru'   => "TRUST",
                         'lkorokh\@gmail\.com'   => "TRUST",
                     );
+    my $ra = $self->TicketObj->RequestorAddresses;
+    RT::Logger->info("Routing ticket for $ra");
 
     #Check each of our defined domains for a match
     foreach my $domainKey (keys %domain_map ){
-    if($self->TicketObj->RequestorAddresses =~ /^.*?${domainKey}/) {
-        # Domain matches - move to the right queue
-        $self->TicketObj->SetQueue($domain_map{$domainKey});
+        RT::Logger->info("Matching $ra with $domainKey");
+
+        if($self->TicketObj->RequestorAddresses =~ /^.*?${domainKey}/) {
+            RT::Logger->info("Matching $ra with $domainKey");
+            # Domain matches - move to the right queue
+            $self->TicketObj->SetQueue($domain_map{$domainKey});
         }
     }
     return 1; # True if action was successful
